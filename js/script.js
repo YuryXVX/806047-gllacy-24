bbc836e4fe9f617a600f73a333643702f7c9da66
YuryXVX <ass_is_out@mail.eu>
2019-03-12 00:04:06 +0300

    Добавлена анимация, отредактирован скрипт

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* Popup */
 
+var overlay = document.querySelector(".modal-overlay");
 var link = document.querySelector(".popup-open-button");
 var popup = document.querySelector(".modal-content");
 var close = popup.querySelector(".modal-close");
@@ -20,6 +21,7 @@ try {
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
+  overlay.classList.add("modal-overlay-show");
   popup.classList.add("modal-show");
   if (storage) {
     login.value = storage;
@@ -32,12 +34,15 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  overlay.classList.remove("modal-overlay-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value || !message.value) {
     evt.preventDefault();
-    console.log("Нужно ввести логин/эл. почту/сообщение");
+    popup.classList.add("modal-error");
+    overlay.classList.add("modal-error");
     } else {
       if (isStorageSupport) {
         localStorage.setItem("login", login.value);
@@ -50,6 +55,7 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
+        overlay.classList.remove("modal-overlay-show");
     }
   }
 });

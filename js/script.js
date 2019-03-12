96afc9964a840d1d6d948cde9b1c374d247dfac7
YuryXVX <ass_is_out@mail.eu>
2019-03-12 21:28:33 +0300

    Добавлена анимация + мницифиц. скрита и стилей

--- js/script.js
+++ js/script.js
@@ -31,23 +31,21 @@ link.addEventListener("click", function (evt) {
     }
 });
 
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-  overlay.classList.remove("modal-overlay-show");
-  popup.classList.remove("modal-error");
-});
-
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value || !message.value) {
     evt.preventDefault();
     popup.classList.add("modal-error");
-    overlay.classList.add("modal-error");
     } else {
       if (isStorageSupport) {
         localStorage.setItem("login", login.value);
     }
   }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  overlay.classList.remove("modal-overlay-show");
 });
 
 window.addEventListener("keydown", function (evt) {
